c8b65adb963ffd01c61037f2cbf8da14a05299e0
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-08-05 13:28:23 +0200

    mal functions list in libft.h

--- libft.h
+++ libft.h
@@ -6,15 +6,18 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 13:04:10 by jlorette          #+#    #+#             */
-/*   Updated: 2024/08/05 13:08:36 by jlorette         ###   ########.fr       */
+/*   Updated: 2024/08/05 13:27:24 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isdigit(char c);
 int	ft_isalpha(int c);
 int	ft_isalnum(int i);
+int	ft_isascii(int i);
+int	ft_isdigit(char c);
+int	ft_isprint(int i);
+int	ft_strlen(char *str);
 
 #endif
