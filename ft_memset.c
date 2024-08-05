64961a8016c8321a1fa611feea138283beb2e19c
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-08-05 15:23:29 +0200

    ft_menset

--- ft_memset.c
+++ ft_memset.c
@@ -1,27 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/05 13:04:10 by jlorette          #+#    #+#             */
-/*   Updated: 2024/08/05 15:22:44 by jlorette         ###   ########.fr       */
+/*   Created: 2024/08/05 15:01:55 by jlorette          #+#    #+#             */
+/*   Updated: 2024/08/05 15:23:06 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*str;
 
-int		ft_isalpha(int c);
-int		ft_isalnum(int i);
-int		ft_isascii(int i);
-int		ft_isdigit(char c);
-int		ft_isprint(int i);
-void	*ft_memset(void *s, int c, size_t n);
-int		ft_strlen(char *str);
-
-#endif
+	i = 0;
+	str = (char *)s;
+	while (i < n)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (s);
+}
