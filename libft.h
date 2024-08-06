f4bcf0de498e37d10f6c30bbcac24795e82bb34c
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-08-06 18:16:31 +0200

    fix ft_isdigit

--- libft.h
+++ libft.h
@@ -21,7 +21,7 @@ void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(int i);
 int		ft_isalpha(int c);
 int		ft_isascii(int i);
-int		ft_isdigit(char c);
+int		ft_isdigit(int c);
 int		ft_isprint(int i);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
