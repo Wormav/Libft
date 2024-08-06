3fb3c4e154852d121ab386c7df6b6db1bd1e8231
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-08-06 13:22:45 +0200

    ft_tolower, ft_toupper

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 13:04:10 by jlorette          #+#    #+#             */
-/*   Updated: 2024/08/05 20:41:58 by jlorette         ###   ########.fr       */
+/*   Updated: 2024/08/06 13:21:41 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,7 @@ size_t	ft_strlcat(char *dest, const char *src, unsigned int size);
 size_t	ft_strlcpy(char *dest, const char *src, unsigned int size);
 int		ft_strlen(const char *str);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
