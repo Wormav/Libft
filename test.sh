b5d2b277677e0d231225c0d2e57f2aac62950dd7
Jérémy Lorette <jeremy.lorette@outlook.com>
2024-08-08 00:57:59 +0200

    test.sh remove -g

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 make
 
 # Compile the test file with the library and execute the test
-cc -Wall -Wextra -Werror tests_libft.c libft.a -g -o tests_libft && ./tests_libft
+cc -Wall -Wextra -Werror tests_libft.c libft.a -o tests_libft && ./tests_libft
 
 # Clean the project using Makefile
 make fclean
