9764986d15d0a4d51b959e863b652e1b43d6c2c4
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-03-12 15:02:36 +0100

    fix calloc in lstnew

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 16:24:49 by jlorette          #+#    #+#             */
-/*   Updated: 2025/03/12 14:39:41 by jlorette         ###   ########.fr       */
+/*   Updated: 2025/03/12 15:02:26 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
-	new = ft_calloc(sizeof(t_list), 0);
+	new = ft_calloc(sizeof(t_list), 1);
 	if (!new)
 		return (NULL);
 	new->content = content;
